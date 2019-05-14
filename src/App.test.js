1c397dc442c4ab8d3bd7903104b5f656b2f6d750
nevets615 <surj316@yahoo.com>
2019-05-14 18:25:48 -0400

    done with app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import App from "./App";
+import * as rtl from "react-testing-library";
+import "jest-dom/extend-expect";
+
+afterEach(rtl.cleanup);
+
+it("renders without crashing", () => {
+  const wrapper = rtl.render(
+    <span className="greet">hello world</span>
+  );
+});
+
+it("check number of buttons", () => {
+
+});
+
+it("check the state of balls and strikes on load", () => {
+
+});
+
+it("check the operation of the balls button", () => {
+
+});
+
+it("check the operation of the foul button", () => {
+
+});
+
+it("check the operation of the strike button", () => {
+
+});
+
+it("check the operation of the hit button", () => {
+
 });
