3d5549a82ef654bab18a59c7c82c657c4b7e7edb
nevets615 <surj316@yahoo.com>
2019-05-14 18:46:48 -0400

    working on tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,38 +1,28 @@
-
 import React from "react";
 import Dashboard from "./components/Dashboard";
 import App from "./App";
 import * as rtl from "react-testing-library";
 import "jest-dom/extend-expect";
+import { render } from "react-testing-library";
+
 
 afterEach(rtl.cleanup);
 
-it("renders without crashing", () => {
-  const wrapper = rtl.render(
-    <span className="greet">hello world</span>
-  );
-});
 
-it("check number of buttons", () => {
+describe("<App />", () => {
+  it("renders without crashing", () => {
+  render(<App />)
+  });
 
-});
+  it("check number of buttons", () => {});
 
-it("check the state of balls and strikes on load", () => {
+  it("check the state of balls and strikes on load", () => {});
 
-});
+  it("check the operation of the balls button", () => {});
 
-it("check the operation of the balls button", () => {
+  it("check the operation of the foul button", () => {});
 
-});
+  it("check the operation of the strike button", () => {});
 
-it("check the operation of the foul button", () => {
-
-});
-
-it("check the operation of the strike button", () => {
-
-});
-
-it("check the operation of the hit button", () => {
-
+  it("check the operation of the hit button", () => {});
 });
